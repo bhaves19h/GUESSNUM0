95cbad35a26397871723464fd14f499c860d6289
bhaves19h <162792561+bhaves19h@users.noreply.github.com>
2024-03-09 15:44:06 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+let num=25;
+let usernum = prompt("gues the number");
+
+while(usernum !=num){
+    usernum =prompt("wrong number GUess agian");
+};
+if (usernum==num){
+console.log("Congratulatons");
+};
